b28c69dd6ba0f8f7c865ff52fdc52808ca2e1162
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-08-29 15:49:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ streamlit.header("The fruit load list contains: ")
 streamlit.dataframe(my_data_row)
 
 
-add_my_fruit = streamlit.text_input("Add a fruit", add_my_fruit)
+add_my_fruit = streamlit.text_input("Add a fruit")
