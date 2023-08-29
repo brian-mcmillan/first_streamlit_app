818925a565f5f662157387245de00dde386e2714
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-08-29 15:45:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,6 @@ streamlit.text(my_data_row)
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains: ")
+streamlit.dataframe(my_data_row)
 
